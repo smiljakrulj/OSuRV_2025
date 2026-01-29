e44c18c1ee54f9f68b3a71b70d2201088dd27b42
Milos Subotic <milos.subotic.sm@gmail.com>
2026-01-29 10:11:30 +0100

    Tabs.

--- Garbage_Sort/SW/App/3_Wiper_Limit_SW/wiper_limit_switch_node.c
+++ Garbage_Sort/SW/App/3_Wiper_Limit_SW/wiper_limit_switch_node.c
@@ -11,7 +11,7 @@
 
 int main()
 {
-    int fd,r;
+	int fd,r;
 	fd = open(DEV_STREAM_FN, O_RDWR);
 	if(fd < 0){
 		fprintf(stderr, "ERROR: \"%s\" not opened!\n", DEV_STREAM_FN);
@@ -19,11 +19,11 @@ int main()
 		return 4;
 	}
 
-    uint8_t first_command[3] = {'w', 4, 1}; 
-    uint8_t second_command[3] = {'w', 3, 0}; 
-    uint8_t third_command[3] = {'w', 2, 1};
-    uint8_t fourth_command[2] = {'d', 22};
-    uint8_t fifth_command[3] = {'w', 2, 0};
+	uint8_t first_command[3] = {'w', 4, 1}; 
+	uint8_t second_command[3] = {'w', 3, 0}; 
+	uint8_t third_command[3] = {'w', 2, 1};
+	uint8_t fourth_command[2] = {'d', 22};
+	uint8_t fifth_command[3] = {'w', 2, 0};
 
 
 	r = write(fd, first_command, sizeof(first_command));
@@ -31,66 +31,66 @@ int main()
 		fprintf(stderr, "ERROR: write went wrong!\n");
 		return 4;
 	}
-    usleep(100000);
+	usleep(100000);
 
-    printf("Stigli smo do ovde : 1\n");
+	printf("Stigli smo do ovde : 1\n");
 
-    r = write(fd, second_command, sizeof(second_command));
+	r = write(fd, second_command, sizeof(second_command));
 	if(r != sizeof(second_command)){
 		fprintf(stderr, "ERROR: write went wrong!\n");
 		return 4;
 	}
-    usleep(100000);
+	usleep(100000);
 
-    printf("Stigli smo do ovde: 2\n");
+	printf("Stigli smo do ovde: 2\n");
 
-    r = write(fd, third_command, sizeof(third_command));
+	r = write(fd, third_command, sizeof(third_command));
 	if(r != sizeof(third_command)){
 		fprintf(stderr, "ERROR: write went wrong!\n");
 		return 4;
 	}
-    usleep(100000); 
+	usleep(100000); 
 
 
-    printf("Stigli smo do ovde : 3\n");
+	printf("Stigli smo do ovde : 3\n");
 
-    uint8_t rd_val=0;
+	uint8_t rd_val=0;
 
-    while(1)
-    {
-        r = write(fd, fourth_command, sizeof(fourth_command));
-	    if(r != sizeof(fourth_command)){
-		    fprintf(stderr, "ERROR: write went wrong!\n");
-		    return 4;
-	    }
-        usleep(100); 
+	while(1)
+	{
+		r = write(fd, fourth_command, sizeof(fourth_command));
+		if(r != sizeof(fourth_command)){
+			fprintf(stderr, "ERROR: write went wrong!\n");
+			return 4;
+		}
+		usleep(100); 
 
 		r = read(fd, (char*)&rd_val, sizeof(rd_val));
-        if(r != sizeof(rd_val)){
+		if(r != sizeof(rd_val)){
 			fprintf(stderr, "ERROR: read went wrong!\n");
 			return 5;
 		}
 
 
-        if(rd_val)
-        {
-            printf("Doslo je do stanja 1\n");
-            break;
-        }
+		if(rd_val)
+		{
+			printf("Doslo je do stanja 1\n");
+			break;
+		}
 
-        printf("Trenutna vrednost je nula\n");
+		printf("Trenutna vrednost je nula\n");
 
-    }
+	}
 
-    r = write(fd, fifth_command, sizeof(fifth_command));
+	r = write(fd, fifth_command, sizeof(fifth_command));
 	if(r != sizeof(fifth_command)){
-	    fprintf(stderr, "ERROR: write went wrong!\n");
-	    return 4;
-    }
+		fprintf(stderr, "ERROR: write went wrong!\n");
+		return 4;
+	}
 
 
-    printf("Zavrsili smo sa rotiranjem\n");
-    close(fd);
+	printf("Zavrsili smo sa rotiranjem\n");
+	close(fd);
 
-    return 0;
+	return 0;
 }
